3012278a9a071689651ba8616e413f4ab8656962
Dr.Li <ldr0531@gmail.com>
2015-05-22 00:01:17 +0800

    Add coming soon page

---
+++ app.js
@@ -0,0 +1,64 @@
+var port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    mime = require('mime');
+//    html = fs.readFileSync('index.html');
+
+var log = function (entry) {
+    //fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+    console.log(entry);
+};
+
+var send404 = function (response) {
+    response.writeHead(404, {"Content-type": "text/plain"});
+    response.write("404 not found");
+    response.end();
+};
+
+var sendPage = function (response, filePath, fileContents) {
+    log(filePath);
+    log(path.basename(filePath));
+    log(mime.lookup(path.basename(filePath)));
+    response.writeHead(200, {"Content-type": mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+};
+
+var serverWorking = function (response, absPath) {
+    fs.exists(absPath, function (exists) {
+        if (exists) {
+            fs.readFile(absPath, function (err, data) {
+                if (err) {
+                    log("Error occured" + err);
+                    send404(response);
+                } else {
+                    sendPage(response, absPath, data);
+                }
+            });
+        } else {
+            log("Request for non-existing resource: " + absPath);
+            send404(response);
+        }
+    })
+};
+
+var server = http.createServer(function (request, response) {
+    var filePath;
+
+    if ('GET' == request.method ) {
+        if ('/' == request.url ) {
+            filePath = "public/index.html";
+        } else {
+            filePath = "public" + request.url;
+        }
+    }
+
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
